c268b8192ced297aa767a1d0628d83e2116a5bbc
Dmitrii Arsenov <dmitry.arsyonov@mail.ru>
2026-05-29 22:43:01 +0200

    fix 10.05 point 1 att 32

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,28 @@
 # frozen_string_literal: true
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :github, ENV.fetch('GITHUB_CLIENT_ID', nil), ENV.fetch('GITHUB_CLIENT_SECRET', nil), scope: 'user'
+  provider :github, ENV.fetch('GITHUB_CLIENT_ID', ''), ENV.fetch('GITHUB_CLIENT_SECRET', ''), scope: 'user'
+end
+
+if Rails.env.test?
+  OmniAuth.config.test_mode = true
+
+  # Placeholder AuthHash: before_callback_phase заменит его реальными данными из params.
+  OmniAuth.config.mock_auth[:github] = OmniAuth::AuthHash.new(
+    provider: 'github',
+    uid: '',
+    info: { email: '', name: '' },
+    extra: { admin: false }
+  )
+
+  OmniAuth.config.before_callback_phase do |env|
+    params = env['omniauth.params'] || {}
+    email = params['email'] || 'user@test.com'
+    env['omniauth.auth'] = OmniAuth::AuthHash.new(
+      provider: 'github',
+      uid: email,
+      info: { email: email, name: email.split('@').first },
+      extra: { admin: params['admin'] == 'true' }
+    )
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   scope module: :web do
     root 'bulletins#index'
     post 'auth/:provider', to: 'auth#request', as: :auth_request
-    get 'auth/:provider/callback', to: 'auth#callback', as: :callback_auth
+    get 'auth/:provider/callback', to: 'auth#callback', as: :callback_auth, via: %i[get post]
     delete '/logout', to: 'auth#logout', as: :logout
     resources :bulletins do
       member do
