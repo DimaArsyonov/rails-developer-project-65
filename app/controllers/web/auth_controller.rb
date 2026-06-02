0620431bcf5cfc07ca4a0422eb2dfed0ed966626
Dmitrii Arsenov <dmitry.arsyonov@mail.ru>
2026-06-02 21:01:13 +0200

    fix 10.05 point 1 att 42

--- app/controllers/web/auth_controller.rb
+++ app/controllers/web/auth_controller.rb
@@ -3,7 +3,7 @@
 class Web::AuthController < Web::ApplicationController
   def callback
     auth = request.env['omniauth.auth']
-    user = User.find_or_create_by(email: auth['info']['email'], name: auth['info']['name'])
+    user = User.find_or_create_by(email: auth['info']['email'])
     session[:user_id] = user.id
     redirect_to root_path, notice: "#{t(:signed_in)} #{auth['provider']}!"
   end
