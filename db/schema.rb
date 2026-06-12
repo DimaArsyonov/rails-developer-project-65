7631d8a165fd42bd4815e7292b87cabb7826948e
Dmitrii Arsenov <dmitry.arsyonov@mail.ru>
2026-06-12 17:10:06 +0200

    fix 10.05 final attempt 1

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2026_06_11_190830) do
+ActiveRecord::Schema[7.2].define(version: 2026_06_12_150906) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -63,7 +63,7 @@ ActiveRecord::Schema[7.2].define(version: 2026_06_11_190830) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.boolean "admin", default: false, null: false
+    t.boolean "admin", default: true, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
